9fead958d2cded7341efbe5189a7d73693857b29
matt <matt@multicode.co.uk>
2025-04-14 11:36:54 +0100

    03 Beans

--- 03-Beans/quickstartdemo/src/main/java/com/neueda/quickstartdemo/QuickstartdemoApplication.java
+++ 03-Beans/quickstartdemo/src/main/java/com/neueda/quickstartdemo/QuickstartdemoApplication.java
@@ -2,12 +2,16 @@ package com.neueda.quickstartdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class QuickstartdemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(QuickstartdemoApplication.class, args);
+		ApplicationContext context = SpringApplication.run(QuickstartdemoApplication.class, args);
+		PasswordGenerator bean = context.getBean(PasswordGenerator.class);
+		System.out.println("Bean as created at start up: " + bean.getClass());
+		System.out.println(bean.getNewPassword());
 	}
 
 }
